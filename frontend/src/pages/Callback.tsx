c8ecbfe601043f4d2211acb02e21b3237b31d4c0
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-04 22:19:19 +0900

    feat: Login

--- frontend/src/pages/Callback.tsx
+++ frontend/src/pages/Callback.tsx
@@ -1,33 +1,42 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { JwtPayload, jwtDecode } from "jwt-decode";
 
 function Callback() {
   const [searchParams, setSearchParams] = useSearchParams();
   const code = searchParams.get("code");
-  const [userData, setUserData] = useState<ResLogin>();
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(
       `http://${process.env.REACT_APP_BASE_URL}/api/auth/google?code=${code}`
     )
       .then((res) => {
-        if (!res.ok || res.status === 500) throw new Error("다시 시도해주세요");
+        if (!res.ok || res.status === 500) {
+          navigate("/");
+          throw new Error("다시 시도해주세요");
+        }
         return res.json();
       })
-      .then((data) => setUserData(data))
-      .catch((err) => alert(err));
+      .then((data) => {
+        localStorage.setItem("id", data.id);
+        localStorage.setItem("name", data.name);
+        localStorage.setItem("nickname", data.nickname);
+        localStorage.setItem("access_token", data.access_token);
+        const decoded = jwtDecode<JwtPayload>(data.access_token);
+        const exp = decoded.exp?.toString() as string;
+        localStorage.setItem("access_token_exp", exp);
+        //todo
+        // localStorage.setItem("avatar", `/api/user/${userId}/photo?timestamp=${Date.now()}`);
+        localStorage.setItem("isLoggedIn", "true");
+
+        alert("로그인에 성공하였습니다.");
+        navigate("/");
+      })
+      .catch((err) => alert(err.message));
   }, [code]);
 
-  return (
-    <div>
-      <li>${userData?.message}</li>
-      <li>${userData?.id}</li>
-      <li>${userData?.name}</li>
-      <li>${userData?.email}</li>
-      <li>${userData?.nickname}</li>
-      <li>${userData?.access_token}</li>
-    </div>
-  );
+  return <h1>유저 정보를 불러오는 중입니다</h1>;
 }
 
 export default Callback;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import googleLoginImg from "../assets/google_login.svg";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn") === "true") {
+      alert("이미 로그인되어 있습니다.");
+      navigate("/");
+    }
+  }, []);
+
   return (
     <a
       href={`https://accounts.google.com/o/oauth2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&response_type=${process.env.REACT_APP_GOOGLE_RESPONSE_TYPE}&scope=email+profile`}
