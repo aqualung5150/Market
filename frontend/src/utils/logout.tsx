eecbfd884b923b82d7e0fe126df63d367e937905
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-05 22:31:40 +0900

    fix: 로그아웃 시 alert 제거

--- frontend/src/utils/logout.tsx
+++ frontend/src/utils/logout.tsx
@@ -10,7 +10,6 @@ const logout = async () => {
     method: "POST",
   })
     .then(() => {
-      alert("로그아웃 되었습니다.");
       window.location.href = "/";
     })
     .catch((err) => alert(err.message));
