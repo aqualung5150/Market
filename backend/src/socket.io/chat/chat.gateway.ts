07782f6431bc13c50c0a34d6639178eb6ffc0b15
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-14 15:31:59 +0900

    feat: 채팅 구조

--- backend/src/socket.io/chat/chat.gateway.ts
+++ backend/src/socket.io/chat/chat.gateway.ts
@@ -1,7 +1,6 @@
 import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
-  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
@@ -39,8 +38,37 @@ export class ChatGateway
   //   client.emit('hello', 'client: ' + data + '/server: yes you.');
   // }
 
-  @SubscribeMessage('sendMessage')
-  handleSendMessage(client: ChatSocket, payload) {
-    this.logger.debug(payload.body + ' - ' + payload.channelId);
+  @SubscribeMessage('getChannelsReq')
+  // 클라이언트가 속한 모든 채널을 조회하면서 room에 join 시켜줘야함
+  handleGetChannelsReq(client: ChatSocket) {
+    this.logger.debug('getChannelsReq');
+  }
+
+  @SubscribeMessage('getMessagesReq')
+  handleGetMessagesReq(client: ChatSocket) {
+    this.logger.debug('getMessagesReq');
+  }
+
+  @SubscribeMessage('sendMessageReq')
+  handleSendMessageReq(client: ChatSocket, payload) {
+    this.io
+      .to(`${payload.channelId}`)
+      .emit('sendMessageRes', payload.body + ' - ' + payload.channelId);
+  }
+
+  @SubscribeMessage('newChannelReq')
+  handleNewChannelReq(client: ChatSocket, payload) {
+    /*
+    payload = {
+      toUser: number
+      message: string
+    }
+
+    1. 새로운 채널 생성
+    2. 채널에 client와 toUser
+      -> toUser의 소켓id를 어디서 찾을것인가
+      1. 소켓에 연결될때 db에 저장 -> 디스크
+      2. map<userId, User> 로 저장해둘 것인가 -> 메모리
+    */
   }
 }
