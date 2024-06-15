e4305a53a563ae67cc5f9c4550403030f118f068
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-15 23:26:08 +0900

    feat: 소켓 데이터 타입

--- backend/src/@types/socket.d.ts
+++ backend/src/@types/socket.d.ts
@@ -9,3 +9,27 @@ interface ChatSocket extends Socket {
 }
 
 // interface ChatSocket extends Socket, ChatUser {}
+
+//////////////
+
+interface SocketUserData {
+  id: number;
+  nickname: string;
+}
+
+interface SocketMessageData {
+  id: number;
+  body: string;
+  read: boolean;
+  createdAt: string;
+  channelId: number;
+  sender: SocketUserData;
+}
+
+interface SocketChannelData {
+  id: number;
+  lastMessage: string;
+  lastMessageDate: string;
+  read: boolean;
+  users: SocketUserData[];
+}
