ffcbd3afb918eef4555a0b5e281de76c6f757be6
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-16 13:49:19 +0900

    refactor

--- backend/src/@types/socket.d.ts
+++ backend/src/@types/socket.d.ts
@@ -2,15 +2,7 @@ import { Socket } from 'socket.io';
 
 interface ChatSocket extends Socket {
   userId: number;
-  // name: string;
-  // email: string;
-  // nickname: string;
-  // curRoom: string;
 }
-
-// interface ChatSocket extends Socket, ChatUser {}
-
-//////////////
 
 interface SocketUserData {
   id: number;
@@ -18,7 +10,7 @@ interface SocketUserData {
 }
 
 interface SocketMessageData {
-  // id: number;
+  id: number;
   body: string;
   read: boolean;
   createdAt: string;
