c802a2858295b12857c15d6d9dcaa8213ac61548
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-25 20:06:26 +0900

    feat

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,13 @@
+const Loading = () => {
+  return (
+    <div className="h-full w-full flex flex-col justify-center items-center">
+      <div
+        className="h-12 w-12 animate-spin rounded-full border-amber-400 border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      ></div>
+      <strong className="mt-4 text-2xl">Loading...</strong>
+    </div>
+  );
+};
+
+export default Loading;
