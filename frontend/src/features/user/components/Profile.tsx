e1d1849dea8195646a7e6df20e245fe7d0329b5f
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-25 21:48:48 +0900

    feat: useAxios

--- frontend/src/features/user/components/Profile.tsx
+++ frontend/src/features/user/components/Profile.tsx
@@ -1,54 +1,50 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../app/store";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { axiosInstance } from "../../../data/axiosInstance";
-import { User } from "../../../@types/user";
+import { useNavigate, useParams } from "react-router-dom";
 import Button from "../../../components/Button";
 import { setOpenChat, setSendTo } from "../../chat/chatSlice";
+import useAxios from "../../../hooks/useAxios";
+import Loading from "../../../components/Loading";
 
 const Profile = () => {
   const paramId = useParams().id;
   const me = useSelector((state: RootState) => state.user);
-  const [profileData, setProfileData] = useState<User>();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { data, error, loading } = useAxios(`users/${paramId}`);
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const res = await axiosInstance.get(`users/${paramId}`);
-        setProfileData(res.data);
-      } catch (err) {
-        alert("유저 정보를 불러오지 못했습니다");
-      }
-    };
-    getUserData();
-  }, []);
-
-  if (!profileData) return null;
+  if (error) {
+    alert("유저 정보를 불러오지 못했습니다.");
+    navigate(-1);
+  }
 
   return (
-    <div className="flex flex-col flex-1 items-center">
-      <div className="flex m-10 w-2/3 min-h-[300px] border rounded-md">
-        <div className="w-1/5 p-5">
-          <img
-            className=" w-16 h-16 min-w-16 rounded-full object-cover"
-            src={`${process.env.REACT_APP_API_URL}/users/profileImage/${profileData.image}`}
-          />
+    <>
+      {loading && <Loading />}
+      {data && (
+        <div className="flex flex-col flex-1 items-center">
+          <div className="flex m-10 w-2/3 min-h-[300px] border rounded-md">
+            <div className="w-1/5 p-5">
+              <img
+                className=" w-16 h-16 min-w-16 rounded-full object-cover"
+                src={`${process.env.REACT_APP_API_URL}/users/profileImage/${data.image}`}
+              />
+            </div>
+            <div className="p-5 flex-1">{data.nickname}</div>
+            <div>{paramId}</div>
+          </div>
+          {me.id !== data.id && (
+            <Button
+              text="채팅하기"
+              onClick={() => {
+                dispatch(setSendTo(data.id));
+                dispatch(setOpenChat(true));
+              }}
+            />
+          )}
         </div>
-        <div className="p-5 flex-1">{profileData.nickname}</div>
-        <div>{paramId}</div>
-      </div>
-      {me.id !== profileData.id && (
-        <Button
-          text="채팅하기"
-          onClick={() => {
-            dispatch(setSendTo(profileData.id));
-            dispatch(setOpenChat(true));
-          }}
-        />
       )}
-    </div>
+    </>
   );
 };
 
---
+++ frontend/src/hooks/useAxios.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../data/axiosInstance";
+
+const useAxios = (url: string) => {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const res = await axiosInstance.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+
+  return { data, error, loading };
+};
+
+export default useAxios;
