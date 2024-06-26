991c0130d30e0c2e12385a3e43dad216e6050ed5
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-26 16:20:20 +0900

    feat: 삭제버튼 및 썸네일 설정

---
+++ frontend/src/features/product/components/form/ImageSelector.tsx
@@ -0,0 +1,57 @@
+import { useRef } from "react";
+import { ReactComponent as CameraIcon } from "../../../../assets/camera.svg";
+
+const ImageSelector = ({
+  files,
+  urls,
+  setFiles,
+  setUrls,
+  handleFilesChange,
+}: ImageSelectorProps) => {
+  const selectFile = useRef<HTMLInputElement>(null);
+
+  const deleteImage = (idx: number) => {
+    setFiles((prev) => prev.filter((_, i) => i !== idx));
+    setUrls((prev) => prev.filter((_, i) => i !== idx));
+  };
+
+  return (
+    <div className="flex">
+      <input
+        type="file"
+        className="hidden"
+        accept={"image/png, image/gif, image/jpeg"}
+        onChange={handleFilesChange}
+        multiple
+        ref={selectFile}
+      />
+      <div
+        onClick={() => selectFile.current?.click()}
+        className="flex flex-col justify-center items-center min-w-24 w-24 h-24 rounded bg-gray-300 cursor-pointer"
+      >
+        <CameraIcon className="pt-1 w-8 h-8" />
+        <span className="text-xs">{files.length} / 5</span>
+      </div>
+      {urls.map((image, idx) => (
+        <div className="relative ml-1">
+          {idx === 0 && (
+            <div className="absolute top-0 left-0 min-w-24 w-24 h-24 rounded border-green-500 border-2 bg-transparent"></div>
+          )}
+          <img
+            className="max-w-24 w-24 h-24 rounded object-cover"
+            key={idx}
+            src={image}
+          />
+          <div
+            className="absolute border w-5 h-5 text-xs bg-white rounded-full top-1 right-1 flex justify-center items-center cursor-pointer"
+            onClick={() => deleteImage(idx)}
+          >
+            X
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ImageSelector;
