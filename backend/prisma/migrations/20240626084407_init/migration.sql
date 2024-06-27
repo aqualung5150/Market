ece69fe70e17dfa25cf34116ea319b024a0045e9
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-27 12:22:28 +0900

    feat: Product Model

---
+++ backend/prisma/migrations/20240626084407_init/migration.sql
@@ -0,0 +1,16 @@
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- CreateTable
+CREATE TABLE "ProductImage" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "order" SMALLINT NOT NULL,
+    "main" BOOLEAN NOT NULL,
+    "productId" INTEGER NOT NULL,
+
+    CONSTRAINT "ProductImage_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProductImage" ADD CONSTRAINT "ProductImage_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
