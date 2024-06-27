72b0d0fbff688694a2addccc88bc429df74ca7f8
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-27 12:22:11 +0900

    feat: Validation Pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { SocketIOAdapter } from './socket.io/socket-io-adapter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   app.use(cookieParser());
   app.useWebSocketAdapter(new SocketIOAdapter(app));
   await app.listen(3001);
