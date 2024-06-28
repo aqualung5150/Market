689958631afecce840cb52090d70aad8146750e6
SeungJoonChoi <tmdwns0845@naver.com>
2024-06-28 18:41:19 +0900

    fix: Unique child key

--- frontend/src/features/product/components/category/CategoryDropdown.tsx
+++ frontend/src/features/product/components/category/CategoryDropdown.tsx
@@ -4,10 +4,11 @@ import categoryData from "../../data/category.json";
 const CategoryDropdown = () => {
   const navigate = useNavigate();
   return (
-    <div className="absolute min-w-[100px] w-[140px] top-full left-0 bg-white border rounded shadow select-none">
+    <div className="absolute z-10 min-w-[100px] w-[140px] top-full left-0 bg-white border rounded shadow select-none">
       <ul className="text-sm  font-normal p-2 space-y-2 cursor-pointer">
         {Object.values(categoryData).map((category) => (
           <li
+            key={category.id}
             onClick={() => navigate(`/product/search?category=${category.id}`)}
           >
             {category.label}
