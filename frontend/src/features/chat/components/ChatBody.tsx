b3f0317fa9575aa5cc581e7f25408bca2ff5f932
SeungJoonChoi <tmdwns0845@naver.com>
2024-07-02 13:20:59 +0900

    remove

--- frontend/src/features/chat/components/ChatBody.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ChatBodyProps } from "../../../types/chat";
-
-const ChatBody = ({ children }: ChatBodyProps) => {
-  return <div className="flex h-full overflow-auto">{children}</div>;
-};
-
-export default React.memo(ChatBody);
