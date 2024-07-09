fac49278a9b5b45ec75a34400d8613b2dea0c737
SeungJoonChoi <tmdwns0845@naver.com>
2024-07-09 22:32:57 +0900

    feat: Display Sold Out

--- frontend/src/features/product/components/productThumbnail/ProductThumbnail.tsx
+++ frontend/src/features/product/components/productThumbnail/ProductThumbnail.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ProductData } from "types/product";
 import timeAgo from "utils/timeAgo";
+import { ReactComponent as CheckIcon } from "assets/check.svg";
 
 const ProductThumbnail = ({ product }: { product: ProductData }) => {
   return (
@@ -9,12 +10,22 @@ const ProductThumbnail = ({ product }: { product: ProductData }) => {
       to={`/product/${product.id}`}
       className="flex aspect-[3/5] w-full cursor-pointer flex-col rounded-lg border bg-white shadow-md lg:aspect-[2/3]"
     >
-      <img
-        className="aspect-square rounded-t-lg object-cover"
-        src={`${process.env.REACT_APP_API_URL}/product/productImage/${
-          product.images.find((e) => e.main)?.url
-        }`}
-      />
+      <div className="relative aspect-square rounded-t-lg">
+        <img
+          className="aspect-square rounded-t-lg object-cover"
+          src={`${process.env.REACT_APP_API_URL}/product/productImage/${
+            product.images.find((e) => e.main)?.url
+          }`}
+        />
+        {product.status === 1 && (
+          <div className="absolute left-0 top-0 flex aspect-square w-full flex-col items-center justify-center gap-2 rounded-t-lg bg-black/50">
+            <CheckIcon className="h-16 w-16 stroke-white/85 lg:h-24 lg:w-24" />
+            <span className="text-base font-bold text-white/85 lg:text-2xl">
+              판매완료
+            </span>
+          </div>
+        )}
+      </div>
       <div className="flex w-full flex-1 flex-col p-2">
         <div className="flex-1 space-y-1">
           <div className="text-base font-semibold lg:text-lg">
