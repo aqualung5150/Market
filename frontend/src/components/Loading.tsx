7184dfcaa3e7a58e77329e5ea21121ea17e36332
SeungJoonChoi <tmdwns0845@naver.com>
2024-07-20 16:03:01 +0900

    refactor

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,10 +1,7 @@
 const Loading = ({ text }: { text: string }) => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
-      <div
-        className="h-12 w-12 animate-spin rounded-full border-4 border-solid border-amber-400 border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status"
-      ></div>
+      <p className="h-12 w-12 animate-spin rounded-full border-4 border-black border-b-transparent" />
       <strong className="mt-4 text-2xl">{text}</strong>
     </div>
   );
