12dca1b3d6b45faa8a0183da20c8bf0867ac4762
SeungJoonChoi <tmdwns0845@naver.com>
2024-07-20 16:03:20 +0900

    feat: Link to Seller Profile

--- frontend/src/features/product/components/productDetail/SellerInfo.tsx
+++ frontend/src/features/product/components/productDetail/SellerInfo.tsx
@@ -1,13 +1,16 @@
 import SendButton from "features/chat/components/SendButton";
+import { Link } from "react-router-dom";
 import { SellerInfoProps } from "types/product";
 
 const SellerInfo = ({ seller }: SellerInfoProps) => {
   return (
     <div className="min-h-[200px] w-full flex-1">
       <div className="border-b pb-2 text-3xl">판매자</div>
-      <div className="flex w-full items-center gap-2 pt-2">
-        <span className="text-xl font-bold">{seller.nickname}</span>
-        <span className="flex-1 text-gray-500">#{seller.id}</span>
+      <div className="flex w-full items-center justify-between pt-2">
+        <Link to={`/users/${seller.id}`}>
+          <span className="mr-1 text-xl font-bold">{seller.nickname}</span>
+          <span className="text-gray-500">#{seller.id}</span>
+        </Link>
         <SendButton
           className="h-10 w-20 rounded bg-green-500 font-bold text-white"
           text="채팅하기"
