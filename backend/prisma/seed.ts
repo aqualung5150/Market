cf77e5ab7a4f1f2c25b97aba2651370bba4e1653
SeungJoonChoi <tmdwns0845@naver.com>
2024-07-22 19:57:38 +0900

    feat: DB seeding

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcryptjs';
+/*
+To resolve json file
+-> tsconfig.json
+"esModuleInterop": true,
+"resolveJsonModule": true
+*/
+import categoryData from '../data/category.json';
+
+const prisma = new PrismaClient();
+
+async function hash(text: string) {
+  const saltOrRounds = 10;
+  const hashedToken = await bcrypt.hash(text, saltOrRounds);
+  return hashedToken;
+}
+
+async function main() {
+  return prisma.$transaction(async (tx) => {
+    // create admin user
+    const hashedPassword = await hash(process.env.ADMIN_PASSWORD);
+    await tx.user.upsert({
+      where: {
+        id: 1,
+      },
+      update: {},
+      create: {
+        id: 1,
+        email: 'admin',
+        nickname: 'admin',
+        password: hashedPassword,
+      },
+    });
+
+    const categories = Object.values(categoryData);
+    for (const category of categories) {
+      await tx.category.upsert({
+        where: {
+          id: category.id,
+        },
+        update: {},
+        create: {
+          id: category.id,
+          title: category.label,
+        },
+      });
+    }
+  });
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(() => prisma.$disconnect());
